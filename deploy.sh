45cdad92eeb3998fd9cfc2239f9013cb695083c0
GuGu910 <gugu_mani@yahoo.com>
2019-07-20 20:34:11 +0530

    removed ingress.yaml in deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,3 @@ sudo kubectl apply -f CDPipe/deployment.yaml
 
 # Creating appflask kubernet service
 sudo kubectl apply -f CDPipe/service.yaml
-
-# Creating Ingress for helloworld service
-sudo kubectl apply -f CDPipe/ingress.yaml
